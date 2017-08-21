50f76d41b484e2f3da826ad1e10a4d45567f518c
Thuyvk's Blog <khanhthuy46k2dhv@gmail.com>
2017-08-21 17:02:57 +0700

    ng-event

---
+++ AngularJs_StepByStep/Scripts/RepeatController.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="angular.js" />
+
+(function () {
+    'use strict';
+    var myApp = angular.module('app', []);
+    myApp.controller('RepeatController', ['$scope', function ($scope) {
+        $scope.message = "Hello world";
+        var employees = [
+            { name: 'Join Smirt', age: '18', address: '21 main street' },
+            { name: 'Maria Ozawa', age: '17', address: '07 Anapolist' }
+        ];
+        $scope.employees = employees;
+    }]);
+
+})();
