10bc868e1828ebd57509b3ba090e07cc5e341c36
Thuyvk's Blog <khanhthuy46k2dhv@gmail.com>
2017-08-25 15:49:10 +0700

    validation

---
+++ AngularJs_StepByStep/validation/myValidationController.js
@@ -0,0 +1,5 @@
+/// <reference path="D:\GitHubs_Project\AngularSample\AngularJs_StepByStep\Scripts/angular.js" />
+/// <reference path="D:\GitHubs_Project\AngularSample\AngularJs_StepByStep\Scripts/app.js" />
+myApp.controller('myValidationController',['$scope',function($scope){
+    $scope.message = "Angular Validation";
+}]);
