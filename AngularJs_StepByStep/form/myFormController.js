43de00bb82cb7bc022fd0870a5699578e0571f4d
Thuyvk's Blog <khanhthuy46k2dhv@gmail.com>
2017-08-25 09:57:26 +0700

    angular form example

--- AngularJs_StepByStep/form/myFormController.js
+++ AngularJs_StepByStep/form/myFormController.js
@@ -1,5 +1,28 @@
 ﻿/// <reference path="D:\GitHubs_Project\AngularSample\AngularJs_StepByStep\Scripts/angular.js" />
 /// <reference path="D:\GitHubs_Project\AngularSample\AngularJs_StepByStep\Scripts/app.js" />
-myApp.controller('myFormController', ['$scope', function ($scope) {
-    $scope.message = "Hello AngularJS Form";
+
+myApp.controller('myFormController', ['$scope', '$http', '$log', function ($scope, $http, $log) {
+    $scope.message = "AngularJS Form";
+    //init data
+    $scope.initStudent = {
+        firstName: 'Join',
+        lastName: 'Smirt',
+        dob: new Date('02/07/1987'),
+        traningType: 'online',
+        maths : false,
+        physics: false,
+        chemistry: false,
+        gender: 'male'
+    };
+
+    //fill data for First Load
+    $scope.student = angular.copy($scope.initStudent);
+
+    $scope.SubmitForm = function(){
+        $log.info($scope.student);
+    };
+
+    $scope.ResetForm = function(){
+        $scope.student = angular.copy($scope.initStudent);
+    };
 }]);
